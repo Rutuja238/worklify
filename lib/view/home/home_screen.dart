944632b5653bc5292ef4b6baff5ac2c1e948a1f7
Rutuja238 <narrutuja@gmail.com>
2025-04-11 16:58:40 +0530

    bottom navbar add

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../../viewmodel/home/home_viewmodel.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  final User? user = FirebaseAuth.instance.currentUser;
 
   @override
   Widget build(BuildContext context) {
-    final vm = Provider.of<HomeViewModel>(context);
+    String displayName = user?.displayName ?? "User";
 
     return Scaffold(
+      backgroundColor: const Color(0xFFF5F5FF),
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Text('Dashboard', style: TextStyle(color: Colors.black)),
-        actions: [
+        title: Text("Hi, $displayName!"),
+         actions: [
           IconButton(
             icon: const Icon(Icons.person, color: Colors.black),
             onPressed: () {
@@ -21,10 +28,67 @@ class HomeScreen extends StatelessWidget {
             },
           ),
         ],
+        backgroundColor: Colors.white,
         elevation: 0,
+        foregroundColor: Colors.black,
       ),
       body: Center(
-        child: Text('Welcome, ${vm.user?.displayName ?? "User"}!'),
+        child: Text(
+          "Welcome, $displayName",
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _navIcon(Icons.home_outlined, 0),
+            _navIcon(Icons.chat_bubble_outline, 1),
+            _buildMiddleButton(), // Plus button
+            _navIcon(Icons.calendar_today_outlined, 2),
+            _navIcon(Icons.person_outline, 3),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _navIcon(IconData icon, int index) {
+    return IconButton(
+      icon: Icon(
+        icon,
+        color: _selectedIndex == index ? Colors.deepPurple : Colors.grey,
+        size: 26,
+      ),
+      onPressed: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+    );
+  }
+
+  Widget _buildMiddleButton() {
+    return Container(
+      height: 58,
+      width: 58,
+      decoration: const BoxDecoration(
+        color: Colors.deepPurple,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          )
+        ],
+      ),
+      child: IconButton(
+        icon: const Icon(Icons.add, color: Colors.white, size: 30),
+        onPressed: () {
+          // Handle add action
+        },
       ),
     );
   }
